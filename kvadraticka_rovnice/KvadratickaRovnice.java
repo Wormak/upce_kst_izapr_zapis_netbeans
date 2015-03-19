89d721a998a1c7470fd14bbb44e5dd9404a5f9f8
pdostal <git@pdostal.cz>
2015-03-19 15:24:47 +0100

    Přidat některé podmínky

--- kvadraticka_rovnice/KvadratickaRovnice.java
+++ kvadraticka_rovnice/KvadratickaRovnice.java
@@ -12,10 +12,24 @@ public class KvadratickaRovnice {
       double c = (double)sc.nextInt();
 
       double d = Math.pow(b, 2)-4*a*c;
-      double x1 = -b + Math.sqrt(d) / 2 * a;
-      double x2 = -b - Math.sqrt(d) / 2 * a;
 
-      System.out.println("x = "+x1);
-      System.out.println("x = "+x2);
+      double x1 = 0;
+      double x2 = 0;
+      if ( d > 0 ) {
+        x1 = -b + Math.sqrt(d) / 2 * a;
+        x2 = -b - Math.sqrt(d) / 2 * a;
+      } else if ( d == 0 ) {
+        x1 = -b / 2 * a;
+      }
+
+      System.out.println("d = " + d);
+      if ( d > 0 ) {
+        System.out.println("x = " + x1);
+        System.out.println("x = " + x2);
+      } else if ( d == 0 ) {
+        System.out.println("x = " + x1);
+      } else {
+        System.out.println("Rovnice nemá řešení.");
+      }
     }
 }
