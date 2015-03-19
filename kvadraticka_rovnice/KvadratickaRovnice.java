185c99ff5437a9ed956f48a97ee18d65c80c1022
pdostal <git@pdostal.cz>
2015-03-19 14:52:37 +0100

    Kvadraticka rovnice

--- kvadraticka_rovnice/KvadratickaRovnice.java
+++ kvadraticka_rovnice/KvadratickaRovnice.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class KvadratickaRovnice {
     public static void main(String[] args) {
-      System.out.println("START");
+      Scanner sc = new Scanner(System.in);
 
-      
+      System.out.print("a = ");
+      double a = (double)sc.nextInt();
+      System.out.print("b = ");
+      double b = (double)sc.nextInt();
+      System.out.print("c = ");
+      double c = (double)sc.nextInt();
 
-      System.out.println("STOP");
+      double d = Math.pow(b, 2)-4*a*c;
+      double x1 = -b + Math.sqrt(d) / 2 * a;
+      double x2 = -b - Math.sqrt(d) / 2 * a;
+
+      System.out.println("x = "+x1);
+      System.out.println("x = "+x2);
     }
 }
