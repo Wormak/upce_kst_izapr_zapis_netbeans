d0e996e0095a640a76ff5ed88241ce41a89eea4d
pdostal <git@pdostal.cz>
2015-03-26 14:21:45 +0100

    Move everything out of directories and destroy Netbeans stuff

--- Sample_project.java
+++ Sample_project.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package sample_project;
-
 /**
  *
  * @author pavel
@@ -17,5 +10,5 @@ public class Sample_project {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
