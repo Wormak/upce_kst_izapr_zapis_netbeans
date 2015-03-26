51fe4129230f6c9de203ac475c6b665461ec9d6a
pdostal <git@pdostal.cz>
2015-03-26 15:40:10 +0100

    Spolecny delitel

---
+++ Spolecny_delitel.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Spolecny_delitel {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("a = ");
+    int min = sc.nextInt();
+    System.out.print("b = ");
+    int max = sc.nextInt();
+
+    while ( a != b ) {
+      if ( a > b ) {
+        a -= b;
+      } else {
+        b -= a;
+      }
+      int x = ;
+    }
+
+    System.out.println("x = "+x);
+  }
+}
